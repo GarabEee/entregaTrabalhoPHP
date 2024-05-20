0d1a430110d19f3e33d79462c017b3a388cdd28b
GarabEee <133824758+GarabEee@users.noreply.github.com>
2024-05-20 20:26:04 -0300

    Update conectaBanco.php

--- entregaTrabalho1/conectaBanco.php
+++ entregaTrabalho1/conectaBanco.php
@@ -1,6 +1,6 @@
 <?php
     $banco = new mysqli(
-        "localhost", "root", "", "maxSneakers", "3306"
+        "localhost", "root", "Senha123", "maxSneakers", "3306"
     );
     if($banco->connect_errno){
         echo "Erro ao conectar no banco de dados";
